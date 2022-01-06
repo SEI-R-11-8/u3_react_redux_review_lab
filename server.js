510d99164dbf21e1026b68e58751363d3d20d63b
vladkim91 <vkcoding21@gmail.com>
2022-01-06 07:57:14 -0800

    server.js and mongoDB connected

--- server.js
+++ server.js
@@ -1,2 +1,31 @@
+const express = require('express');
+const routes = require('./routes');
+const db = require('./db');
+const bodyParser = require('body-parser');
 const logger = require('morgan');
+const path = require('path');
+
+// require('dotenv').config({ path: './.env' });
+
+// require() imports and middleware here ^ ///////
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+app.use(bodyParser.json());
 app.use(logger('dev'));
+
+// app.use() middleware here ^ ///////////////////
+
+app.use('/api', routes);
+
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, 'client/build')));
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.join(`${__dirname}/client/build/index.html`));
+//   });
+// }
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
