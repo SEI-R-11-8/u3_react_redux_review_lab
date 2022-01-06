06a3be63ce5bcd8aa7e7b5a9c4467a4c2fda2c93
Michael Newman <therealmichaelnewman@gmail.com>
2022-01-06 16:05:26 -0500

    created Home and CityDetail components

---
+++ client/src/components/CityDetail.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function CityDetail(props) {
+    console.log(props)
+    return (
+        <div>
+            <h1>City Detail page</h1>
+        </div>
+    )
+}
