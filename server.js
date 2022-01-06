9fd22c17b650bf766c040f87bdeb0803e9f09685
andora814 <smith.doran@gmail.com>
2022-01-06 12:08:47 -0500

    beach controllers all tested on insomnia

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const express = require('express');
 const routes = require('./routes');
 const db = require('./db');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
 app.use(cors());
-
+app.use(bodyParser.json());
 app.use('/', routes);
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
