b19b7ca2c96e3f607ae33260418f276848b1a0e6
Pablo Yocupicio <Pabloey@yahoo.com>
2022-01-06 08:06:51 -0700

    Models and index all set

--- models/comment.js
+++ models/comment.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose')
 
 const Comment = new mongoose.Schema(
   {
-    name: { type: 'string', required: true },
-    text: { type: 'string', required: true },
+    name: { type: String, required: true },
+    text: { type: String, required: true },
     rating: { type: Number, required: true },
     id: { type: mongoose.Schema.Types.ObjectId, ref: 'posts' },
   },
-  {timestamps: true}
+  { timestamps: true }
 )
 
 module.exports = Comment;
--- models/post.js
+++ models/post.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const Post = new mongoose.Schema(
   {
-    name: { type: String, required: true },
+    title: { type: String, required: true },
     image: { type: String, required: true },
     description: { type: String, required: true },
     price: { type: Number, required: true },
