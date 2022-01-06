81dbbaf6ceba33fc0d4bf738056a7867f296a1c7
Michael Newman <therealmichaelnewman@gmail.com>
2022-01-06 15:08:21 -0500

    created backend CRUD functionality

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,4 @@
-const { City } = require("../models/index");
+const { City, Comment, Review } = require("../models/index");
 
 const getAllCities = async (req, res) => {
   try {
@@ -9,6 +9,88 @@ const getAllCities = async (req, res) => {
   }
 };
 
+/////////////
+
+const postComment = async (req, res) => {
+  try {
+    const comment = await new Comment(req.body);
+    await comment.save();
+    return res.status(201).json({ comment });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
+const getAllComments = async (req, res) => {
+  try {
+    const comments = await Comment.find();
+    return res.status(200).json({ comments });
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+};
+
+const updateComment = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Comment.findByIdAndUpdate(
+      id,
+      req.body,
+      { new: true },
+      (err, comment) => {
+        if (err) {
+          res.status(500).send(err);
+        }
+        if (!comment) {
+          res.status(500).send("Comment not found!");
+        }
+      }
+    );
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+};
+
+const deleteComment = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleted = await Comment.findByIdAndDelete(id);
+    if (deleted) {
+      return res.status(200).send("Comment deleted");
+    }
+    throw new Error("Comment not found");
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+};
+
+////////////////////
+
+const getAllReviews = async (req, res) => {
+  try {
+    const reviews = await Review.find();
+    return res.status(200).json({ reviews });
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+};
+
+const postReview = async (req, res) => {
+  try {
+    const review = await new Review(req.body);
+    await review.save();
+    return res.status(201).json({ review });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
 module.exports = {
   getAllCities,
+  getAllComments,
+  getAllReviews,
+  postReview,
+  postComment,
+  updateComment,
+  deleteComment,
 };
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,25 @@ const { Router } = require("express");
 const router = Router();
 const controllers = require("../controllers/index");
 
+////////
+
 router.get("/", (req, res) => res.send("This is root!"));
 
 router.get("/cities", controllers.getAllCities);
+
+//////
+router.post("/comments", controllers.postComment);
+
+router.get("/comments", controllers.getAllComments);
+
+router.put("/comments/:id", controllers.updateComment);
+
+router.delete("/comments/:id", controllers.deleteComment);
+
+//////////
+
+router.post("/reviews", controllers.postReview);
+
+router.get("/reviews", controllers.getAllReviews);
 
 module.exports = router;
