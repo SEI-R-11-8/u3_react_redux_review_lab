936662775fab244def91270a06ff6b9c89905995
Aaron Slaughter <aslaugh1@gmail.com>
2022-01-06 11:55:58 -0500

    added routes for posts and comments

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,15 @@
 const { Router } = require('express')
-// const controllers = require('../controllers')
+const controllers = require('../controllers')
 const router = Router()
 
 router.get('/', (req, res) => res.send('This is root!'))
+
+router.post('/posts', controllers.createPost)
+router.get('/posts/:id', controllers.getPostbyId)
+router.get('/posts', controllers.getAllPosts)
+router.put('/posts/:id', controllers.updatePost)
+
+router.post('/comments', controllers.createComment)
+router.put('/comments/:id', controllers.updateComment)
 
 module.exports = router
