7f31f4ad17d53b13565be56911bd2c77d0ecb49c
Pablo Yocupicio <Pabloey@yahoo.com>
2022-01-06 10:13:19 -0700

    Getting one specific comment working

--- controllers/index.js
+++ controllers/index.js
@@ -11,6 +11,17 @@ const getAllPosts = async (req, res) => {
   }
 }
 
+const getOneComment = async (req, res) => {
+  try {
+    const comment = await Comment.find({ _id: req.params.id });
+    return res.status(201).json({
+      comment
+    })
+  } catch (error) {
+    return res.status(500).json({ error: error.message })
+  }
+}
+
 const getComments = async (req, res) => {
   try {
     const comments = await Comment.find({ id: req.params.id });
@@ -43,9 +54,20 @@ const postComment = async (req, res) => {
   }
 }
 
+const updateComment = async (req, res) => {
+  try {
+    const comment = await Comment.findByIdAndUpdate(req.param.id, req.body, { new: true })
+    return res.status(201).json({})
+  } catch (error) {
+    return res.status(500).json({ error: error.message })
+  }
+}
+
 module.exports = {
   getAllPosts,
   getComments,
   deleteComment,
   postComment,
+  updateComment,
+  getOneComment
 }
--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,12 @@ router.get('/', (req, res) => res.send('This is root'))
 
 router.get('/posts', controllers.getAllPosts)
 router.get('/posts/:id', controllers.getComments)
+router.get('/comment/:id', controllers.getOneComment)
 
 router.delete('/posts/:id', controllers.deleteComment)
 
 router.post('/posts/:id', controllers.postComment)
+
+router.put('/posts/:id', controllers.updateComment)
 
 module.exports = router;
