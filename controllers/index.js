df428e9e18363badc821dd8c5fa7e3c00670422d
Michael Newman <therealmichaelnewman@gmail.com>
2022-01-06 12:35:15 -0500

    created getAllCities controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,14 @@
-const getCities = (req, res) => {
-  res.send({
-    message: "Getting cities",
-  });
+const { City } = require("../models/index");
+
+const getAllCities = async (req, res) => {
+  try {
+    const cities = await City.find();
+    return res.status(200).json({ cities });
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
 };
 
 module.exports = {
-  getCities,
+  getAllCities,
 };
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const { Router } = require("express");
 const router = Router();
+const controllers = require("../controllers/index");
 
 router.get("/", (req, res) => res.send("This is root!"));
+
+router.get("/cities", controllers.getAllCities);
 
 module.exports = router;
