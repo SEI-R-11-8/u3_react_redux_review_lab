a085f23c39a780de5b5b80684b3612d39873147b
Aaron Slaughter <aslaugh1@gmail.com>
2022-01-06 15:10:03 -0500

    created post state reducer

---
+++ client/src/store/reducers/PostReducer.js
@@ -0,0 +1,24 @@
+import { GET_POSTS, NEW_POST } from "../types"
+
+const initialState = {
+  posts: [],
+  newPost: {
+    city: '',
+    date: '',
+    likes: 0,
+    description: ''
+  }
+}
+
+const PostReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GET_POSTS:
+      return { ...state, posts: action.payload }
+    case NEW_POST:
+      return { ...state, newPost: action.payload }
+    default:
+      return { ...state }  
+  }
+}
+
+export default PostReducer
