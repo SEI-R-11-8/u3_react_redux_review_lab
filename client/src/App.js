dd53dcda579984f53c8e63be72a203e097b6fcf7
Aaron Slaughter <aslaugh1@gmail.com>
2022-01-06 23:59:35 -0500

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
 import PostPage from './components/PostPage';
+import { Route, Switch } from 'react-router'
+import PostDetails from './components/PostDetails';
 
 function App() {
   return (
     <div className="App">
-      <PostPage/>
+      <Switch>
+        
+        <Route exact path="/" render={(props) =>
+          <PostPage {...props}/>}>
+        </Route>
+
+        <Route path="/:id" render={(props) => 
+          <PostDetails {...props}/>
+        }>
+        </Route>
+
+      </Switch>
+      
     </div>
   )
 }
