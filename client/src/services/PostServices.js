b68f3370f6c088f37d550db135a22e2888024e06
Aaron Slaughter <aslaugh1@gmail.com>
2022-01-06 22:54:03 -0500

    added get a post calls to local db

--- client/src/services/PostServices.js
+++ client/src/services/PostServices.js
@@ -3,7 +3,15 @@ import { PostsClient } from './index'
 export const GetPosts = async () => {
   try {
     const response = await PostsClient.get(`/posts`)
-    return response.data.posts
+    return response.data.posts.reverse()
+  } catch (error) {
+    throw error
+  }
+}
+
+export const PostNewPost = async (newPost) => {
+  try {
+    await PostsClient.post(`/posts`, newPost)
   } catch (error) {
     throw error
   }
