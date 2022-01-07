9df54fb5e7b82c9f9cbe89ba85f65aca3ff7f427
Aaron Slaughter <aslaugh1@gmail.com>
2022-01-07 00:00:27 -0500

    added style classes

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const PostList = (props) => {
 
   return (
-    <div>
+    <div className="post-card">
       {props.posts.map((element, index) => 
         <div key={index}>
-          <img src={element.photo} alt=""/>
+          <Link to={`/${element._id}`}><img className="main-image" src={element.photo} alt=""/></Link>
           <p>{element.city}</p>
           <p>{element.date}</p>
           <p>{element.description}</p>
