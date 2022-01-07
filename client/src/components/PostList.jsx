cead05838ed3cb6029498c99aa612da17ae16763
Aaron Slaughter <aslaugh1@gmail.com>
2022-01-06 22:59:32 -0500

    sectioned off the mapping of the posts into this list

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -1,18 +1,12 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { useEffect } from 'react'
-import { LoadPosts } from '../store/actions/PostActions'
 
-export const PostList = (props) => {
-
-  useEffect(() => {
-    props.fetchPosts()
-  }, [])
+const PostList = (props) => {
 
   return (
     <div>
-      {props.postState.posts.map((element, index) => 
+      {props.posts.map((element, index) => 
         <div key={index}>
+          <img src={element.photo} alt=""/>
           <p>{element.city}</p>
           <p>{element.date}</p>
           <p>{element.description}</p>
@@ -23,14 +17,4 @@ export const PostList = (props) => {
   )
 }
 
-const mapStateToProps = ({ postState }) => {
-  return { postState }
-}
-
-const mapDispatchToProps = (dispatch) => {
-  return {
-    fetchPosts: () => dispatch(LoadPosts())
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(PostList)
+export default PostList
