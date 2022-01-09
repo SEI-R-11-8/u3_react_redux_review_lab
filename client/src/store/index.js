83c22f6026cd5d07daac041356525ac45c7cd2a5
BenjaminoBambino <benjaminlpeck@gmail.com>
2022-01-08 18:26:21 -0800

    set up store/index.js

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,5 +1,14 @@
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import LocationsReducer from './reducers/LocationsReducers';
+import CommentsReducer from './reducers/CommentsReducer';
 
-const store = createStore(() => ({}));
+const store = createStore(
+  combineReducers({
+    locationsState: LocationsReducer,
+    commentsState: CommentsReducer
+  }),
+  composeWithDevTools()
+);
 
 export default store;
